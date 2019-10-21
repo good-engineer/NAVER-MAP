d083f19671498ad7b2c816b6e182a7fcf9fa5a19
good-engineer <ms_danesh@hotmail.com>
2019-10-21 23:32:52 +0900

    added fragment manager and current location button

--- NaverMap/app/src/main/java/com/naver/navermap/MainActivity.kt
+++ NaverMap/app/src/main/java/com/naver/navermap/MainActivity.kt
@@ -2,11 +2,45 @@ package com.naver.navermap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.fragment.app.FragmentActivity
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.MapFragment
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),OnMapReadyCallback{
+    private lateinit var map:NaverMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        super<AppCompatActivity>.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fm = supportFragmentManager
+        val mapFragment = fm.findFragmentById(R.id.map_fragment) as MapFragment?
+            ?: MapFragment.newInstance().also {
+                fm.beginTransaction().add(R.id.map_fragment, it).commit()
+            }
+
+        mapFragment.getMapAsync(this)
+
     }
+
+
+
+    override fun onMapReady(p0: NaverMap) {
+    map=p0
+        val uiSettings = map.uiSettings
+        uiSettings.isCompassEnabled = false
+        uiSettings.isLocationButtonEnabled = true
+        uiSettings.isIndoorLevelPickerEnabled = true
+        uiSettings.isZoomControlEnabled = true
+
+        // sample for printing 좌표
+        //val coord = LatLng(37.5670135, 126.9783740)
+        //Toast.makeText(applicationContext,"위도: ${coord.latitude}, 경도: ${coord.longitude}", Toast.LENGTH_SHORT).show()
+    }
+
+
 }
+
