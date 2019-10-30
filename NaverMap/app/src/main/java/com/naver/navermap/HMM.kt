31e5a9bdde9ce32474256af423dd28cfff8eff81
good-engineer <ms_danesh@hotmail.com>
2019-10-31 07:56:51 +0900

    update map matching algorithm

--- NaverMap/app/src/main/java/com/naver/navermap/HMM.kt
+++ NaverMap/app/src/main/java/com/naver/navermap/HMM.kt
@@ -2,39 +2,61 @@ package com.naver.navermap
 
 interface RoadState {
     //TODO : implement proper interface
-    fun setCurrentState(state:RoadState)
-    fun addDirectedRoad (road: RoadState)
+    fun setCurrentState(state: RoadState)
+
+    fun addDirectedRoad(road: RoadState)
     fun addOnelinkRoad(road: RoadState)
 
 }
 
-class RoadStateContext{
+
+class RoadStateContext {
     private var currentState: RoadState? = null
     private var length: Int? = 0
-    private val directRoadList : MutableList <RoadState> = arrayListOf()
-    private val oneLinkRoadList : MutableList <RoadState> = arrayListOf()
+    private val directRoadList: MutableList<RoadState> = arrayListOf()
+    private val oneLinkRoadList: MutableList<RoadState> = arrayListOf()
 
     init {
-        //TODO: set initial state accoriding to last known location
-        // TODO: find the connected roads and add
-
+        currentState = InitialState()
     }
 
-    fun setLength(l:Int){
-        length=l
+    fun setLength(l: Int) {
+        length = l
     }
 
-    fun setCurrentState (state: RoadState){
+    fun setCurrentState(state: RoadState) {
         currentState = state
     }
 
-    fun addDirectedRoad (road: RoadState){
+    fun addDirectedRoad(road: RoadState) {
         directRoadList.add(road)
     }
 
-    fun addOnelinkRoad(road: RoadState){
+    fun addOnelinkRoad(road: RoadState) {
         oneLinkRoadList.add(road)
     }
 
-    
+
 }
+
+class InitialState : RoadState {
+
+    //TODO: set initial state accoriding to last known location
+    // TODO: find the connected roads and add
+
+    override fun setCurrentState(state: RoadState) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun addDirectedRoad(road: RoadState) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun addOnelinkRoad(road: RoadState) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+}
+
+
---
+++ NaverMap/app/src/main/java/com/naver/navermap/Viterbi.kt
@@ -0,0 +1,13 @@
+package com.naver.navermap
+//TODO
+/*implement viterbi algorithm
+1. set current location as initial state and find connected roads
+2. get a sequence of location
+3. calculate emission probability
+4. find the best path
+5. update state
+
+*/
+class Viterbi {
+
+}
