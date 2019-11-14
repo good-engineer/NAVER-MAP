f27979d3e89ab7ca7d57eae3dc29dfcd4ad883f9
good-engineer <ms_danesh@hotmail.com>
2019-11-15 07:54:59 +0900

    edited the hmm

--- NaverMap/app/src/main/java/com/naver/navermap/HMM.kt
+++ NaverMap/app/src/main/java/com/naver/navermap/HMM.kt
@@ -1,62 +1,76 @@
 package com.naver.navermap
 
-interface RoadState {
-    //TODO : implement proper interface
-    fun setCurrentState(state: RoadState)
+import android.location.Location
 
-    fun addDirectedRoad(road: RoadState)
-    fun addOnelinkRoad(road: RoadState)
+class RoadState {
+    //TODO : implement Road state
+    private var startPoint: Location? = null
+    private var endPoint: Location? =null
+    private var length: Int?=0
+    //more variable?
+
+    fun RoadState(info:String? ){
+    //get an string containing the road info and set the variables
+    }
+    fun setRoadState(start:Location?, end:Location? ){
+        //set road state
+    }
+    fun getRoadState(){
+        //get road info
+
+    }
+    fun getStartPoint(){
+
+    }
+    fun getEndPoint(){
+
+    }
+    fun getlength(){
+
+    }
 
 }
 
 
-class RoadStateContext {
+class hmm {
     private var currentState: RoadState? = null
-    private var length: Int? = 0
-    private val directRoadList: MutableList<RoadState> = arrayListOf()
-    private val oneLinkRoadList: MutableList<RoadState> = arrayListOf()
+    private val roadSequence: MutableList<RoadState> = arrayListOf()
+    private val transitionProb: MutableList<Int> = arrayListOf()
 
     init {
-        currentState = InitialState()
+        //request last 2 location
+        //set initial curr state by parsing jason data
     }
 
-    fun setLength(l: Int) {
-        length = l
+    fun setRoadSequence(location: Location?){
+        //set a road sequence by parsing jason data in appreciate to current road or location ?
     }
 
-    fun setCurrentState(state: RoadState) {
-        currentState = state
+    fun TransitionProb(location1: Location?,location2: Location?){
+        //get 2 last location and calculate the corresponding prob for every roadstate in list
     }
 
-    fun addDirectedRoad(road: RoadState) {
-        directRoadList.add(road)
+    fun setCurrentState(location1:Location?,location2:Location? ) {
+        //set curr state accoriding to transition probability
+
     }
 
-    fun addOnelinkRoad(road: RoadState) {
-        oneLinkRoadList.add(road)
+    fun getCurrentState(){
+
     }
+    fun getRoadSequence(){
+
+    }
+    fun getTransitionProb(){
+        
+    }
+
+
+
 
 
 }
 
-class InitialState : RoadState {
 
-    //TODO: set initial state accoriding to last known location
-    // TODO: find the connected roads and add
-
-    override fun setCurrentState(state: RoadState) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun addDirectedRoad(road: RoadState) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun addOnelinkRoad(road: RoadState) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-
-}
 
 
