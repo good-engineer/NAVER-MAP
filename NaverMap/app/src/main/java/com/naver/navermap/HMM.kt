fc27a89b797a29c5a7594938e56c550415910284
pjhh88 <pjhh8888@gmail.com>
2019-11-17 01:02:19 +0900

    implement Projection point to line in sphere(earth)

--- NaverMap/app/src/main/java/com/naver/navermap/HMM.kt
+++ NaverMap/app/src/main/java/com/naver/navermap/HMM.kt
@@ -1,35 +1,13 @@
 package com.naver.navermap
 
 import android.location.Location
+import com.naver.maps.geometry.LatLng
 
-class RoadState {
-    //TODO : implement Road state
-    private var startPoint: Location? = null
-    private var endPoint: Location? =null
-    private var length: Int?=0
-    //more variable?
-
-    fun RoadState(info:String? ){
-    //get an string containing the road info and set the variables
-    }
-    fun setRoadState(start:Location?, end:Location? ){
-        //set road state
-    }
-    fun getRoadState(){
-        //get road info
-
-    }
-    fun getStartPoint(){
-
-    }
-    fun getEndPoint(){
-
-    }
-    fun getlength(){
-
-    }
-
-}
+data class RoadState(
+    val startPoint: LatLng,
+    val endPoint: LatLng,
+    val length: Int
+)
 
 
 class hmm {
@@ -42,31 +20,30 @@ class hmm {
         //set initial curr state by parsing jason data
     }
 
-    fun setRoadSequence(location: Location?){
+    fun setRoadSequence(location: Location?) {
         //set a road sequence by parsing jason data in appreciate to current road or location ?
     }
 
-    fun TransitionProb(location1: Location?,location2: Location?){
+    fun TransitionProb(location1: Location?, location2: Location?) {
         //get 2 last location and calculate the corresponding prob for every roadstate in list
     }
 
-    fun setCurrentState(location1:Location?,location2:Location? ) {
+    fun setCurrentState(location1: Location?, location2: Location?) {
         //set curr state accoriding to transition probability
 
     }
 
-    fun getCurrentState(){
+    fun getCurrentState() {
 
     }
-    fun getRoadSequence(){
+
+    fun getRoadSequence() {
 
     }
-    fun getTransitionProb(){
-        
+
+    fun getTransitionProb() {
+
     }
-
-
-
 
 
 }
