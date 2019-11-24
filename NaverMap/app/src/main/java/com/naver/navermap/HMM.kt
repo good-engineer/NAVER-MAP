b84420c4738f1f755eb198aba33b1c5f7db128a9
pjhh88 <pjhh8888@gmail.com>
2019-11-24 23:17:20 +0900

    delete class hmm

--- NaverMap/app/src/main/java/com/naver/navermap/HMM.kt
+++ NaverMap/app/src/main/java/com/naver/navermap/HMM.kt
@@ -1,6 +1,5 @@
 package com.naver.navermap
 
-import android.location.Location
 import com.naver.maps.geometry.LatLng
 
 data class RoadState(
@@ -8,46 +7,3 @@ data class RoadState(
     val endPoint: LatLng,
     val distance: Double
 )
-
-
-class hmm {
-    private var currentState: RoadState? = null
-    private val roadSequence: MutableList<RoadState> = arrayListOf()
-    private val transitionProb: MutableList<Int> = arrayListOf()
-
-    init {
-        //request last 2 location
-        //set initial curr state by parsing jason data
-    }
-
-    fun setRoadSequence(location: Location?) {
-        //set a road sequence by parsing jason data in appreciate to current road or location ?
-    }
-
-    fun TransitionProb(location1: Location?, location2: Location?) {
-        //get 2 last location and calculate the corresponding prob for every roadstate in list
-    }
-
-    fun setCurrentState(location1: Location?, location2: Location?) {
-        //set curr state accoriding to transition probability
-
-    }
-
-    fun getCurrentState() {
-
-    }
-
-    fun getRoadSequence() {
-
-    }
-
-    fun getTransitionProb() {
-
-    }
-
-
-}
-
-
-
-
